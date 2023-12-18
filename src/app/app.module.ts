82606809db61988f0f302afcc54508e1a05bb5c2
Hitesh Makwana <hiteshmakwana1261@gmail.com>
2023-12-18 11:38:21 +0530

    issue fix.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmailService } from './email.service';
-import { HttpClientModule } from '@angular/common/http';//add
+import { HttpClientModule } from '@angular/common/http';
+import { SubmitformComponent } from './submitform/submitform.component';//add
 
 @NgModule({
   declarations: [
     AppComponent,
+    SubmitformComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/submitform/submitform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submitform',
+  templateUrl: './submitform.component.html',
+  styleUrls: ['./submitform.component.scss']
+})
+export class SubmitformComponent {
+
+}
