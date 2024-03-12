3f2dd0e8c915a5ecc7adebdd6a2bf29c44588b00
HiteshMakwana <hiteshmakwana1261@gmail.com>
2024-03-12 11:02:58 +0530

    Auto-generated commit

---
+++ app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmailService } from './email.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule  //add
+  ],
+  providers: [EmailService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
